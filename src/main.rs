6f10923174f1aff90ca0ff7872bc93d673ccd572
BaggyCapybara93 <baggycapybara93@outlook.com>
2026-05-20 22:14:45 -0400

    fix: remove delete command from help

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ fn main() {
             println!("Available commands:");
             println!("  move <src> <dest> [--recursive]");
             println!("  copy <src> <dest>");
-            println!("  delete <target> [--recursive]");
         }
 
         Command::Move { src, dest, recursive } => {
